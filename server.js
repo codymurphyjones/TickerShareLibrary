fd637c5e39288f2c689d04d2a042a0791b8168fd
Cody Jones <codymurphyjones@gmail.com>
2019-10-30 07:26:26 -0500

    Online Ready

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const next = require('next')
+const cors = require('cors')
+const dev = process.env.NODE_ENV !== 'production'
+const port = process.argv[2].replace("$PORT", "3000");;
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+    
+app.prepare()
+.then(() => {
+  const server = express()
+  server.options('*', cors()) 
+  server.get('*', (req, res) => {
+    return handle(req, res)
+  })
+    
+  server.listen(port, (err) => {
+    if (err) throw err
+    console.log('> Ready on http://localhost:3000')
+  })
+})
+.catch((ex) => {
+  console.error(ex.stack)
+  process.exit(1)
+})
